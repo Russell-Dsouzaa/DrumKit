3854eca0bb6566c628a4068df2b5f7b5b8560ec3
Russell-Dsouzaa <rrussell.dsouza@gmail.com>
2024-10-20 22:35:31 +0530

    Fixed a minor bug in script.js and with creating the README.md file
    The minor bug was a spelling mistake. Along the with above mentioned changes, I added comment line in .js file, explaining the code.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# DrumKit
+# DrumKit 
+[DrumKit](./meta.jpg)
+
+<hr><br><br>
+
+## How to Play:
+    - There are 9 buttons onscreen, which aren't clickable(ironic, ik).
+    - Each of the buttons are labelled according to what sounds they make.
+    - To play these sounds, press on the respective buttons on your keyboard.
+    - Go Crazy!
+
+<hr><hr><br><br><br>
+
+Currently, users can play sounds by clicking on the keyboard buttons. In fututre, I have plans on adding a feature wherein, if the user clicks on the button, it plays the sound.
+
+By the way, this entire project, from start to finish, took me about 1.5 hours. It's currently 1:20 a.m. and I'm feeling quite tired. I'm going to bed now. Goodnight, world! (a Hello World! pun)
--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function removeTransition(e) {
     e.target.classList.remove('playing');
 }
 
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    const key = document.querySelector(`div[data-key="${e.code}"]`);
-    if (!audio) return;
+function playSound(e) {                                                   // console.log(e) prints : KeyboardEvent (all key-value pairs such as code, KeyCode, ctrlKey: true/false, altKey: true/false etc)
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);  // this L.O.C. checks whether there is an audio element which has the same value of data-key as that of the div which we triggered
+    const key = document.querySelector(`div[data-key="${e.code}"]`);      // [] is attibute selector
+    if (!audio) return;                                                   // If the corresponding audio element doesn't exist, the function ends (Example, when we press any other key other than a,s,d,f,g,h,j,k,l)
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
+    key.classList.add('playing');      // adds the playing class to the div with data-key = "e.code"
+    audio.currentTime = 0;             // rewinds the audio file back to start position everytime we press the same key
     audio.play();
 }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));    // We can't directly listen for an event on a list(ListNode, they are like arrays) of keys(elements) at once , we have to loop over each key(button) and listen whether the transition has ended or not
 window.addEventListener('keydown',playSound);
